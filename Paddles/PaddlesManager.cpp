c866e07662e1f4e2ca8793f911711b7d53fe8f92
peersmg <peersmg@gmail.com>
2016-08-08 11:54:10 +0100

    Added scoring

--- Paddles/PaddlesManager.cpp
+++ Paddles/PaddlesManager.cpp
@@ -1,7 +1,5 @@
 #include "PaddlesManager.h"
-#include "Ball.h"
 #include "Game.h"
-#include "Paddle.h"
 #include "DrawManager.h"
 #include <string>
 
@@ -20,31 +18,66 @@ void PaddlesManager::Initialise(int optionSelected)
   m_playerOneScore = 0;
   m_playerTwoScore = 0;
 
-  Ball* pBall = new Ball;
-  Game::instance.m_objects.AddObject(pBall);
-  pBall->Initialise();
+  m_optionSelected = optionSelected;
 
-  Paddle* pPaddleOne = new Paddle;
-  Game::instance.m_objects.AddObject(pPaddleOne);
-  pPaddleOne->Initialise(0, pBall);
+  m_leftRect = sf::FloatRect(0, 0, 10, Game::instance.GetWindow().getSize().y);
+  m_rightRect = sf::FloatRect(Game::instance.GetWindow().getSize().x, 0, 10, Game::instance.GetWindow().getSize().y);
 
-  if (optionSelected == 1)
+  m_pBall = new Ball;
+  Game::instance.m_objects.AddObject(m_pBall);
+  m_pBall->Initialise();
+
+  m_pPaddleOne = new Paddle;
+  Game::instance.m_objects.AddObject(m_pPaddleOne);
+  m_pPaddleOne->Initialise(0, m_pBall);
+
+  if (m_optionSelected == 1)
   {
-    Paddle* pPaddleTwo = new Paddle;
-    Game::instance.m_objects.AddObject(pPaddleTwo);
-    pPaddleTwo->Initialise(1, pBall);
+    m_pPaddleTwo = new Paddle;
+    Game::instance.m_objects.AddObject(m_pPaddleTwo);
+    m_pPaddleTwo->Initialise(1, m_pBall);
   }
   else
   {
-    Paddle* pPaddleTwo = new Paddle;
-    Game::instance.m_objects.AddObject(pPaddleTwo);
-    pPaddleTwo->Initialise(3, pBall);
+    m_pPaddleTwo = new Paddle;
+    Game::instance.m_objects.AddObject(m_pPaddleTwo);
+    m_pPaddleTwo->Initialise(3, m_pBall);
   }
 }
 
 void PaddlesManager::Update(float deltaTime)
 {
+  if (m_pBall->GetRect().intersects(m_leftRect))
+  {
+    m_playerTwoScore++;
+    m_pBall->Initialise();
+    m_pPaddleOne->Initialise(0, m_pBall);
+   
+    if (m_optionSelected == 1)
+    {
+      m_pPaddleTwo->Initialise(1, m_pBall);
+    }
+    else
+    {
+      m_pPaddleTwo->Initialise(3, m_pBall);
+    }
+  }
+  else if (m_pBall->GetRect().intersects(m_rightRect))
+  {
+    m_playerOneScore++;
+    m_pBall->Initialise();
 
+    m_pPaddleOne->Initialise(0, m_pBall);
+
+    if (m_optionSelected == 1)
+    {
+      m_pPaddleTwo->Initialise(1, m_pBall);
+    }
+    else
+    {
+      m_pPaddleTwo->Initialise(3, m_pBall);
+    }
+  }
 }
 
 void PaddlesManager::Draw()
--- Paddles/PaddlesManager.h
+++ Paddles/PaddlesManager.h
@@ -1,10 +1,21 @@
 #pragma once
 #include "GameObject.h"
+#include "Ball.h"
+#include "Paddle.h"
+#include "stdafx.h"
+
 class PaddlesManager : public GameObject
 {
 private:
   int m_playerOneScore;
   int m_playerTwoScore;
+  int m_optionSelected;
+
+  sf::FloatRect m_leftRect;
+  sf::FloatRect m_rightRect;
+  Ball* m_pBall;
+  Paddle* m_pPaddleOne;
+  Paddle* m_pPaddleTwo;
 
 public:
   PaddlesManager();
